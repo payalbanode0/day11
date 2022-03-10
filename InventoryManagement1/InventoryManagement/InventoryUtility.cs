cf3682224e8087e8ae4cb6aa1005974c87051f50
payalbanode0 <payalbanode0@gmail.com>
2022-03-10 12:36:10 +0530

    added assignment

---
+++ InventoryManagement1/InventoryManagement/InventoryUtility.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace InventoryManagement
+{
+    class InventoryUtility
+    {
+        //Create List for Rice , Wheat , Pulse
+        public List<Rice> RiceList { get; set; }
+        public List<Wheat> WheatList { get; set; }
+        public List<Pulse> PulseList { get; set; }
+
+
+        public class Rice
+        {
+            public string Name
+            {
+                get;
+                set;
+            }
+            public int Price
+            {
+                get;
+                set;
+            }
+            public int Kg
+            {
+                get;
+                set;
+            }
+
+            public string Type
+            {
+                get;
+                set;
+            }
+        }
+
+        public class Wheat
+        {
+            public string Name
+            {
+                get;
+                set;
+            }
+            public int Price
+            {
+                get;
+                set;
+            }
+            public int Kg
+            {
+                get;
+                set;
+            }
+
+            public string Type
+            {
+                get;
+                set;
+            }
+        }
+
+        public class Pulse
+        {
+            public string Name
+            {
+                get;
+                set;
+            }
+            public int Price
+            {
+                get;
+                set;
+            }
+            public int Kg
+            {
+                get;
+                set;
+            }
+
+            public string Type
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
